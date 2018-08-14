7b71669553f001b59d70c6181e74953975bf9107
Guilherme Augusto <guilherme_amsilva@hotmail.com>
2018-08-14 18:44:46 -0300

    Implementing HTTP verbs

--- dist/meat-api/server/server.js
+++ dist/meat-api/server/server.js
@@ -1,39 +1,27 @@
-import * as restify from 'restify'
-import * as mongoose from 'mongoose'
-import {environment} from '../common/environment'
-import {Router} from '../common/router'
-import {mergePatchBodyParser} from './merge-patch.parser'
-
-export class Server {
-
-    application: restify.Server;
-
-    initializeDb(): mongoose.MongooseThenable{
-        (<any>mongoose).Promise = global.Promise
-        return mongoose.connect(environment.db.url, {
-            useMongoClient: true
-        })
-    }
-
-    initRoutes(routers: Router[]): Promise<any>{
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = __importStar(require("restify"));
+const environment_1 = require("../common/environment");
+class Server {
+    initRoutes(routers) {
         return new Promise((resolve, reject) => {
-            try{
+            try {
                 this.application = restify.createServer({
                     name: 'meat-api',
                     version: '1.0.0'
-                })
-                
-                this.application.use(restify.plugins.queryParser())
-                this.application.use(restify.plugins.bodyParser())
-                this.application.use(mergePatchBodyParser)
-
+                });
+                this.application.use(restify.plugins.queryParser());
                 //routes
-
-                for(let router of routers){
-                    router.applyRoutes(this.application)
+                for (let router of routers) {
+                    router.applyRoutes(this.application);
                 }
-
-
                 /*this.application.get('/hello', (req, resp, next) => {
                     //resp.contentType = 'application/json';
                     //resp.status(400);
@@ -66,16 +54,18 @@ export class Server {
                         })
                         return next()
                     }])*/
-
-                this.application.listen(environment.server.port, () => {
-                    resolve(this.application)
-                })
-            } catch(error){
-                reject(error)
+                this.application.listen(environment_1.environment.server.port, () => {
+                    resolve(this.application);
+                });
             }
-        }) 
+            catch (error) {
+                reject(error);
+            }
+        });
     }
-    bootstrap(routers: Router[] = []): Promise<Server>{
-        return this.initializeDb().then(() => this.initRoutes(routers).then(() => this))
+    bootstrap(routers = []) {
+        return this.initRoutes(routers).then(() => this);
     }
 }
+exports.Server = Server;
+//# sourceMappingURL=server.js.map
